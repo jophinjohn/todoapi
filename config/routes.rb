d66204f199ad3930e9a9f7802615f436005f43de
jophinjohn <jophinjohn@hotmail.com>
2016-01-31 02:34:13 +0000

    Tried and tested checkpoint complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
- 
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  root 'welcome#index'
   namespace :api, defaults: { format: :json } do
      resources :users
    end
